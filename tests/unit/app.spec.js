d280e2aa7e4190dabbbfdcda2b1772d1ff470fea
periscuelo <periscuelo@gmail.com>
2019-07-25 01:48:08 -0300

    Ajustado teste unitário

--- tests/unit/app.spec.js
+++ tests/unit/app.spec.js
@@ -1,5 +1,9 @@
 import { shallowMount } from '@vue/test-utils';
+import Vue from 'vue';
+import VueRouter from 'vue-router';
 import App from '@/App.vue';
+
+Vue.use(VueRouter);
 
 describe('App.vue', () => {
   it('its a Vue Instance', () => {
